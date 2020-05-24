b45d9a0a4192b585d902de83fd2123f45dd7c2e5
TimLiu1115 <qoo44077010@gmail.com>
2020-05-24 22:40:36 +0800

    加入斜向控制

--- motor_test(VNH2SP30)/motor_test/motor_test.ino
+++ motor_test(VNH2SP30)/motor_test/motor_test.ino
@@ -1,10 +1,10 @@
 #define BRAKE    0
-#define MOTOR_1  0
-#define forword  1
-#define reverse  2
-#define right    3
-#define left     4
+#define forword  2
+#define reverse  3
+#define right    4
+#define left     5
 #define CS_THRESHOLD 15   // Definition of safety current (Check: "1.3 Monster Shield Example").
+int MODE;
 
 //MOTOR LF
 #define MOTOR_LF_A1_PIN 24
@@ -71,15 +71,10 @@ void loop()
   
   while(Serial.available())
   {
-    user_input = Serial.read(); //Read user input and trigger appropriate function
+    user_input = Serial.read();
     digitalWrite(EN_LF_PIN, HIGH),digitalWrite(EN_RF_PIN, HIGH),digitalWrite(EN_LR_PIN, HIGH),digitalWrite(EN_RR_PIN, HIGH);
 
-     
-    if (user_input =='l')
-    {
-       Stop();
-    }
-    else if(user_input =='w')
+    if(user_input =='w')
     {
       Forward();
     }
@@ -103,9 +98,19 @@ void loop()
     {
       DecreaseSpeed();
     }
+    else if(user_input =='f')
+    {
+       MODE = 0;
+       Serial.println("正向");
+    }
+    else if(user_input =='g')
+    {
+       MODE = 1;
+       Serial.println("斜向");
+    }
     else
     {
-      Serial.println("Invalid option entered.");
+      Stop();
     }
       
   }
@@ -115,37 +120,35 @@ void Stop()
 {
   Serial.println("Stop");
   usMotor_Status = BRAKE;
-  motorGo(MOTOR_1, usMotor_Status, 0,0,0,0);
+  motorGo(MODE, usMotor_Status, 0,0,0,0);
 }
 
 void Forward()
 {
   Serial.println("Forward");
   usMotor_Status = forword;
-  motorGo(MOTOR_1, usMotor_Status, usSpeed_LF, usSpeed_RF, usSpeed_LR, usSpeed_RR);
-  Serial.print("Speed: ");
-  Serial.println(Speed);
+  motorGo(MODE, usMotor_Status, usSpeed_LF, usSpeed_RF, usSpeed_LR, usSpeed_RR);
 }
 
 void Reverse()
 {
   Serial.println("Reverse");
   usMotor_Status = reverse;
-  motorGo(MOTOR_1, usMotor_Status, usSpeed_LF, usSpeed_RF, usSpeed_LR, usSpeed_RR);
+  motorGo(MODE, usMotor_Status, usSpeed_LF, usSpeed_RF, usSpeed_LR, usSpeed_RR);
 }
 
 void Right()
 {
   Serial.println("Right");
   usMotor_Status = right;
-  motorGo(MOTOR_1, usMotor_Status, usSpeed_LF, usSpeed_RF, usSpeed_LR, usSpeed_RR);
+  motorGo(MODE, usMotor_Status, usSpeed_LF, usSpeed_RF, usSpeed_LR, usSpeed_RR);
 }
 
 void Left()
 {
   Serial.println("Left");
   usMotor_Status = left;
-  motorGo(MOTOR_1, usMotor_Status, usSpeed_LF, usSpeed_RF, usSpeed_LR, usSpeed_RR);
+  motorGo(MODE, usMotor_Status, usSpeed_LF, usSpeed_RF, usSpeed_LR, usSpeed_RR);
 }
 
 void IncreaseSpeed()
@@ -173,7 +176,7 @@ void IncreaseSpeed()
   Serial.print("\t");
   Serial.println(usSpeed_RR);
 
-  motorGo(MOTOR_1, usMotor_Status, usSpeed_LF, usSpeed_RF, usSpeed_LR, usSpeed_RR);
+  motorGo(MODE, usMotor_Status, usSpeed_LF, usSpeed_RF, usSpeed_LR, usSpeed_RR);
 }
 
 void DecreaseSpeed()
@@ -203,12 +206,12 @@ void DecreaseSpeed()
   Serial.println(usSpeed_RR);
   
 
-  motorGo(MOTOR_1, usMotor_Status, usSpeed_LF, usSpeed_RF, usSpeed_LR, usSpeed_RR);
+  motorGo(MODE, usMotor_Status, usSpeed_LF, usSpeed_RF, usSpeed_LR, usSpeed_RR);
 }
 
 void motorGo(uint8_t motor, uint8_t direct, uint8_t pwm_LF, uint8_t pwm_RF, uint8_t pwm_LR, uint8_t pwm_RR)
 {
-  if(motor == MOTOR_1)
+  if(motor == 0)
   {
     if(direct == forword)
     {
@@ -284,10 +287,65 @@ void motorGo(uint8_t motor, uint8_t direct, uint8_t pwm_LF, uint8_t pwm_RF, uint
       digitalWrite(MOTOR_RR_A1_PIN, LOW); 
       digitalWrite(MOTOR_RR_B1_PIN, LOW);            
     }
+   }
+
+   if(motor == 1)
+  {
+    if(direct == forword)
+    {      
+      digitalWrite(MOTOR_RF_A1_PIN, HIGH); 
+      digitalWrite(MOTOR_RF_B1_PIN, LOW);
+      
+      digitalWrite(MOTOR_LR_A1_PIN, LOW); 
+      digitalWrite(MOTOR_LR_B1_PIN, HIGH);
+    }                 
+    
+    else if(direct == reverse)
+    {  
+      digitalWrite(MOTOR_RF_A1_PIN, LOW); 
+      digitalWrite(MOTOR_RF_B1_PIN, HIGH);
+      
+      digitalWrite(MOTOR_LR_A1_PIN, HIGH); 
+      digitalWrite(MOTOR_LR_B1_PIN, LOW);                
+    }
+
+    else if(direct == right)
+    {
+      digitalWrite(MOTOR_LF_A1_PIN, LOW); 
+      digitalWrite(MOTOR_LF_B1_PIN, HIGH);
+
+      digitalWrite(MOTOR_RR_A1_PIN, HIGH); 
+      digitalWrite(MOTOR_RR_B1_PIN, LOW);                   
+    }
+
+    else if(direct == left)
+    {
+      digitalWrite(MOTOR_LF_A1_PIN, HIGH); 
+      digitalWrite(MOTOR_LF_B1_PIN, LOW);
+      
+      digitalWrite(MOTOR_RR_A1_PIN, LOW); 
+      digitalWrite(MOTOR_RR_B1_PIN, HIGH);                 
+    }
+    
+   else
+    {
+      digitalWrite(MOTOR_LF_A1_PIN, LOW); 
+      digitalWrite(MOTOR_LF_B1_PIN, LOW);
+      
+      digitalWrite(MOTOR_RF_A1_PIN, LOW); 
+      digitalWrite(MOTOR_RF_B1_PIN, LOW);
+      
+      digitalWrite(MOTOR_LR_A1_PIN, LOW); 
+      digitalWrite(MOTOR_LR_B1_PIN, LOW);
+      
+      digitalWrite(MOTOR_RR_A1_PIN, LOW); 
+      digitalWrite(MOTOR_RR_B1_PIN, LOW);            
+    }
+   }
     
     analogWrite(PWM_MOTOR_LF, pwm_LF);
     analogWrite(PWM_MOTOR_RF, pwm_RF);
     analogWrite(PWM_MOTOR_LR, pwm_LR);
     analogWrite(PWM_MOTOR_RR, pwm_RR); 
-  }
+  
 }
